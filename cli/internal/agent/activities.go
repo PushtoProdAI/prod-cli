813e833fccff5c3dac1ca8848df9e34f50b3a027
Nathan Stehr <nstehr@meroxa.io>
2025-07-29 17:06:20 -0400

    first pass hooking in to a pricing backend interface

--- cli/internal/agent/activities.go
+++ cli/internal/agent/activities.go
@@ -23,6 +23,7 @@ const (
 	AgentDeployRenderSteps    = "agent.deployRenderSteps"
 	AgentSummarizeDeploySteps = "agent.summarizeDeploySteps"
 	AgentSummarizeError       = "agent.summarizeError"
+	AgentEstimateRenderCosts  = "agent.estimateRenderCosts"
 )
 
 type Activities struct {
@@ -39,6 +40,7 @@ func (a *Activities) Activities() []workflowext.Activity {
 		{Name: AgentSummarizeDeploySteps, ActFunc: a.summarizeDeploySteps},
 		{Name: AgentDeployRenderSteps, ActFunc: a.deployRenderSteps},
 		{Name: AgentSummarizeError, ActFunc: a.summarizeError},
+		{Name: AgentEstimateRenderCosts, ActFunc: a.estimateRenderCosts},
 	}
 }
 
@@ -119,4 +121,13 @@ func (a *Activities) summarizeError(ctx context.Context, error string) (string, 
 		return "", errors.Errorf("failed to summarize error: %w", err)
 	}
 	return summary.Summary, nil
+}
+
+func (a *Activities) estimateRenderCosts(_ context.Context, spec deployment.DeploymentSpec, strategy deployment.DeploymentStrategy) (deployment.CostEstimate, error) {
+	ra := render.NewRenderDeploymentAdapter(a.renderClient)
+	costs, err := ra.EstimateCost(&spec, strategy)
+	if err != nil {
+		return deployment.CostEstimate{}, errors.Errorf("failed to estimate costs: %w", err)
+	}
+	return costs, nil
 }
